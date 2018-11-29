d0bf907427f56a745bdab1868c31cc1f0addc822
Adriano Shimonoe Moreno dos Santos <adriano.shimonoe@gmail.com>
2018-11-29 15:17:19 -0200

    Added bot messages handlers

--- src/start.js
+++ src/start.js
@@ -1,21 +1,22 @@
 const client = require('./auth/login');
 const BotMessages = require('./handlers/richEmbedMessages');
+const config = require('./config/auth');
 
 client.on('message', msg => {
-    if(msg.author.id === client.user.id) {
-        return;
-    } else {
-        const botMsg = new BotMessages();
-        switch (msg.content) {
-            case '!cgbot help':
-                msg.channel.send(botMsg.onHelp());
-            case '!cgbot markets':
-                msg.channel.send(botMsg.onMarkets());
-            case '!cgbot assets':
-                msg.channel.send(botMsg.onAssets());
-            default:
-                msg.channel.send(botMsg.onError());
-        }
-        return;
+    if(msg.author.bot) return;
+
+    const botMsg = new BotMessages();
+    switch (msg.content) {
+        case '!cgbot help':
+            msg.channel.send(botMsg.onHelp());
+            break;
+        case '!cgbot markets':
+            msg.channel.send(botMsg.onMarkets());
+            break;
+        case '!cgbot assets':
+            msg.channel.send(botMsg.onAssets());
+            break;
+        default:
+            msg.channel.send(botMsg.onError());
     }
 });
